3e8f7408be0db43f14b18b3f065c9c452abdc91e
Zionn <andrei@floatingdots.co>
2022-12-07 01:51:32 +0200

    chore: Documentation comment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,3 +5,9 @@
 <div>
   <CardList />
 </div>
+
+<!-- 
+  checkout for archive and wiki api
+        -> https://archive.org/details/wiki-marvelfandomcom 
+        -> https://marvel.fandom.com/api.php
+-->
