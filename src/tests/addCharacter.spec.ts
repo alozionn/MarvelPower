0d7e80092f623a2e37227d1b05eb140908f8bbb8
alozionn <an.pop95@gmail.com>
2023-02-21 21:13:35 +0200

    asd

--- src/tests/addCharacter.spec.ts
+++ src/tests/addCharacter.spec.ts
@@ -3,7 +3,7 @@ import { describe, expect, test } from 'vitest'
 
 describe('Add a character', () => {
   test('It should add the character into the DB', () => {
-    const char = { ...DEFAULT_CHARACTER }
+    const char1 = { ...DEFAULT_CHARACTER }
 
     expect(1 + 2).equal(3)
   })
